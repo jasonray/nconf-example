644f417484fa3fbc51e9c06fdb3df523e79e31f2
Jason Ray <jason.ray@agilex.com>
2014-11-01 10:04:49 -0400

    show using command line

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var nconf = require('nconf');
 
-nconf.argv()
+nconf
+    .argv()
     .env()
     .file({
         file: 'config.json'
     });
+
+console.log('k: ', nconf.get('k'));
+console.log('database: ', nconf.get('database'));
+console.log('database.host: ', nconf.get('database').host);
