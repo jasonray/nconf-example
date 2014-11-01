8d3d41da330a81f13ee747c2fd84231e1302e6ce
Jason Ray <jason.ray@agilex.com>
2014-11-01 09:44:38 -0400

    add test to read complex property

--- test/readfile.js
+++ test/readfile.js
@@ -1,4 +1,4 @@
-exports.readproperty = function(test) {
+exports.readSimpleProperty = function(test) {
 
     var nconf = require('nconf');
 
@@ -7,5 +7,18 @@ exports.readproperty = function(test) {
     });
 
     test.equal(nconf.get('timeout'), 60);
+    test.done();
+}
+
+exports.readComplexProperty = function(test) {
+
+    var nconf = require('nconf');
+
+    nconf.file({
+        file: 'config.json'
+    });
+
+    test.equal(nconf.get("database").host, "127.0.0.1");
+
     test.done();
 }
